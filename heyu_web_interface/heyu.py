d78e38cd5425bd629462097ec1570cbe65d03fbb
krisbeazley@gmail.com <krisbeazley@gmail.com@782135df-d15e-1403-ea62-833ebfeb5310>
2011-12-23 22:41:42 +0000

    /Added auto_refresh, Added three cookie functions, Updated the stdin.keys method

--- heyu_web_interface/heyu.py
+++ heyu_web_interface/heyu.py
@@ -5,49 +5,71 @@
 # For now we define the location of x10config and heyu here
 x10config = "./x10config"
 heyu = "/usr/local/bin/heyu"
+auto_refresh_rate = "10"
 
 # The version of this script
 Heyu_web_interface_version = "11.56_beta_python"
 
 import cgitb, cgi, os, re, subprocess
 
+
 cgitb.enable()
 stdin = cgi.FieldStorage()
 
 
+expires = "expires=01-Jan-2036 12:00:00 GMT"
 
-# Future cookie function will be something like this.
-# Furture version might check the value of the cookies too.  
+ 
 if 'HTTP_COOKIE' not in os.environ.keys():
-    expires = "expires=01-Jan-2036 12:00:00 GMT"
     print "Set-Cookie: Interface_version=", Heyu_web_interface_version, ";", expires
-    # Important! Subprocess is not called we have to set the header Content-type
-    print('Content-type:text/html')
-    print('')
-
-
-# Important! Make sure we have HTTP_COOKIE in the dictionary
-# Furture version might check the value of the cookies too.
-if 'HTTP_COOKIE' in os.environ:
-    # Important! When calling subprocess and running as a CGI, the header Content-type must be set first. Unless
-    # other headers are to be set, however we can handle other headers without the subprocess much easier.
-    subprocess.call(["echo", "Content-type:text/html"], shell=False)
-    subprocess.call(["echo"], shell=False)
-    try:
+    
+try:
+    if 'heyu_status_change' in stdin.keys():
+        #print "<pre>Input: " + name + " value: " + stdin[name].value + "</pre><BR>"
+        subprocess.call(["echo", "Content-type:text/html"], shell=False)
+        subprocess.call(["echo"], shell=False)
         for name in stdin.keys():
-            if name == 'heyu_status_change':
-                # Leave this line for debugging cgi Stdout
-                # print "<pre>Input: " + name + " value: " + stdin[name].value + "</pre><BR>"
+            subprocess.call(stdin[name].value, shell=True,)
+    else:
+        for name in stdin.keys():
+            if stdin[name].value == 'auto_refresh' or stdin[name].value == 'show_all_modules' or stdin[name].value == 'heyu_theme':             
                 try:
-                    # Execute heyu command here
-                    subprocess.call(stdin[name].value, shell=True,)
+                    cookies = [os.environ['HTTP_COOKIE']]  
+                    for x in cookies:	     
+                        for i in stdin.keys():            
+                            if stdin[i].value == 'auto_refresh' and re.search('(auto_refresh=True)', x) is None:
+                                print "Set-Cookie: auto_refresh=True;", expires
+                                auto_refresh = "True"
+                            if stdin[i].value == 'auto_refresh' and re.search('(auto_refresh=False)', x) is None:
+                                print "Set-Cookie: auto_refresh=False;", expires
+                                auto_refresh = "False"   
+                            
+                            if stdin[i].value == 'show_all_modules' and re.search('(heyu_show_all_modules=True)', x) is None:
+                                print "Set-Cookie: heyu_show_all_modules=True;", expires
+                                heyu_show_all_modules = "True"
+                            if stdin[i].value == 'show_all_modules' and re.search('(heyu_show_all_modules=False)', x) is None:
+                                print "Set-Cookie: heyu_show_all_modules=False;", expires
+                                heyu_show_all_modules = "False"  
+                                                     
+                            if stdin[i].value == 'heyu_theme' and re.search('(heyu_theme=default)', x) is None:
+                                print "Set-Cookie: heyu_theme=default;", expires
+                                heyu_theme = "default"
+                            if stdin[i].value == 'heyu_theme' and re.search('(heyu_theme=compact)', x) is None:
+                                print "Set-Cookie: heyu_theme=compact;", expires
+                                heyu_theme = "compact"
                 except:
                     pass
-    except:
-        pass
 
+        print('Content-type:text/html')
+        print('')
+    
+except:
+        print('Content-type:text/html')
+        print('')
 
+    
 # Start HTML, Javascript 
+
 print("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" \"http://www.w3.org/TR/1998/REC-html40-19980424/loose.dtd\">")
 
 print("<html><head><title>Heyu Web Interface v." + Heyu_web_interface_version + "</title>")
@@ -60,7 +82,11 @@ textarea {
 	width: 400px;
 	height: 650px;
 }
-</style>
+</style>""")
+
+print "<script type=text/javascript>refreshInterval=setInterval('ajax_update()',", auto_refresh_rate + "000);</script>"
+
+print("""
 <script type=text/javascript src=javascript/ajax.js></script>
 <script type=text/javascript src=javascript/phone.js></script>
 <script type=text/javascript src=javascript/progressbar.js></script>
@@ -84,15 +110,21 @@ textarea {
 <img src=\"imgs/tool.png\" alt=none class=icons>
 Control Panel</table></button>
 
-<button type=submit name=Button2 value=show_all_modules class=scene_button onclick=\"Status(); show('')\">
+<button type=submit name=Button2 value=show_all_modules class=scene_button onclick=\"Status(); show('This_is_Foobar')\">
 <table class=scene_button><tr><td class=scene_button>
 <img src=\"imgs/horizon.png\" alt=none class=icons>
 Show All Modules</table></button>
 
-<button type=submit name=Button3 value=auto_refresh class=scene_button onclick=\"Status(); show('')\">
+<button id=refresh type=submit name=Button3 value=auto_refresh class=scene_button onclick=\"Status(); show('')\">
 <table class=scene_button><tr><td class=scene_button>
 <img src=\"imgs/reload.png\" alt=none class=icons>
-Auto Refresh</table></button>
+Auto Refresh""")
+try:
+    auto_refresh = auto_refresh.replace("False","")
+    print(auto_refresh.replace("True", auto_refresh_rate + "s"))
+except:
+    pass
+print("""</table></button>
 </table></button></form></table>
 <br>
 
@@ -109,12 +141,11 @@ Auto Refresh</table></button>
 file = open(x10config)
 
 # Set z for formating HTML rows/columns
-z = 1
-
+z = 0
+print "<form method=\"post\">"
 for line in file:
     if re.match('ALIAS', line.upper()) and re.search('(EXCLUDE)', line.upper()) is None:
         
-        # There is probably a better way ...
         a = re.search("ALIAS", line.upper())        
         if a:
             line = line[:a.start()] + line[a.end():]
@@ -149,23 +180,23 @@ for line in file:
         status = status.replace("1","On")
         status = status.replace("0","Off")
         
-        # Call heyu and get time stamp and slice strings.
-        # This is unfinsihed.
+        # Call heyu and get time stamp and slice strings. ** To do **
         timestamp = subprocess.Popen(heyu + " -c " + x10config + " show tstamp " + addr, shell=True, stdout=subprocess.PIPE)
         timestamp = timestamp.communicate()
         timestamp = timestamp[0]
                 
-        print "<form method=\"post\">"
-        print "<button type=submit name=\"heyu_status_change\" value=\"heyu turn", addr, xstatus +  "\" class=scene_button onclick=\"Status(); show('')\"><table class=button><tr><td class=button>" + on_icon + unit
+        print "<button type=submit name=\"heyu_status_change\" value=\"heyu turn", addr, xstatus +  "\"class=scene_button onclick=\"Status(); show('')\"><table class=button><tr><td class=button>" + on_icon + unit
         print "<td class=info>", status, addr, "<br><br></table></button>"
         
         # For html formating rows/columns
         z = z+1
         if z == 3:
             print "<tr>"
+
  
     else:
         pass
         
 file.closed
 print("</form></table></div></body></html>")
+
