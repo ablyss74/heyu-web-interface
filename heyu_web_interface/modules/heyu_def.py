08a0eff7a5b6f2a0c746edad5c96863bfa6d8fc4
krisbeazley <krisbeazley@782135df-d15e-1403-ea62-833ebfeb5310>
2012-01-03 02:45:07 +0000

    add module directory

---
+++ heyu_web_interface/modules/heyu_def.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# Author: 	Kris Beazley
+# Apache License, Version 2.0 http://www.apache.org/licenses/LICENSE-2.0
+
+
+import os
+
+data = os.environ['data']
+
+
+def crontab():
+    print("""
+            <table><tr><td><table class=control_panel><tr>
+		    <tr>
+		    <td><button type=button class=userconfigbutton onclick=\"Status(); show('control_panel_@{crontab}@{cmd1}')\">cmd 1</button>
+		    <td><button type=button class=userconfigbutton onclick=\"Status(); show('control_panel_@{crontab}@{cmd2}')\">cmd 2</button>
+		    <td><button type=button class=userconfigbutton onclick=\"Status(); show('control_panel_@{crontab}@{cmd3}')\">cmd 3</button>
+		    <td><button type=button class=userconfigbutton onclick=\"Status(); show('control_panel_@{crontab}@{cmd4}')\">cmd 4</button>
+	        <td><button type=button class=userconfigbutton onclick=\"Status(); show('control_panel_@{crontab}@{cmd5}')\">cmd 5</button>
+		    </table>
+            """)
+    if 'cmd' in data:
+        print "This is", data, "outside the textarea"
+   
+    print("""<table><tr><td><br>
+            <textarea name=control_panel_save>""")
+    if 'cmd' in data:
+        print "This is", data, "inside the textarea"
+        
+    print "</textarea></table></form>"
+
+
+
